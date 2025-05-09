bf5671037e828da6cd1de508eab741dbf8f6adf0
AditiSolanke2106 <aditisolanke5@gmail.com>
2025-05-09 11:36:57 +0530

    Updated CSS

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+
 import { useAtom } from 'jotai'
 import { counterAtom } from '../atom/atomCounter'
 const Counter = () => {
     const [counter,setCounter]=useAtom(counterAtom)
   return (
-    <div>
-      <h2>{counter}</h2>
-      <button onClick={()=>setCounter(counter+1)}>INCREMENT</button>
-      <button onClick={()=>setCounter(0)}>RESET</button>
-      <button onClick={()=>setCounter(counter-1)}>DECREMENT</button>
+    <div style={{display:"flex",flexDirection:"row",alignItems:"center",justifyContent:"center"}}>
+      <div style={{display:"flex",flexDirection:"column",gap:"10px"}}>
+        <h2 style={{marginLeft:"7rem"}}>{counter}</h2>
+      <button style={{height:"3rem",width:"15rem"}} onClick={()=>setCounter(counter+1)}>INCREMENT</button>
+      <button style={{height:"3rem",width:"15rem"}} onClick={()=>setCounter(0)}>RESET</button>
+      <button style={{height:"3rem",width:"15rem"}} onClick={()=>setCounter(counter-1)}>DECREMENT</button>
+      </div>
     </div>
   )
 }
